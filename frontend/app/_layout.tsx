67876b6c4c4f0f6fc090e3e99e783a21f2662034
Matthias <matthias.begot@gmail.com>
2025-07-02 16:45:55 +0200

    delete useless header

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <AuthProvider>
         <AuthGuard>
-          <Header />
           <Stack>
             <Stack.Screen name="index" options={{ title: 'Home' }} />
             <Stack.Screen name="login" options={{ title: 'Login' }} />
