f5fed052775228b096d81dede47096433b8e437b
Matthias <matthias.begot@gmail.com>
2025-07-02 13:20:58 +0200

    reset frontend

--- front/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-declare const process: { env: { [key: string]: string | undefined } };
-
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8000/api',
-  spotifyRedirectUri: 'myapp://callback',
-  spotifyClientId: process.env['NG_APP_SPOTIFY_CLIENT_ID'] || ''
-};
