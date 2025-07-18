4f3115f68eb8463acfe065b0085899b6e9756554
Matthias.begot <129052228+bgo-mat@users.noreply.github.com>
2025-07-18 18:43:36 +0200

    remove auth guard and secure store

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -7,7 +7,6 @@ import './global.css'
 
 import { useColorScheme } from 'react-native';
 import { AuthProvider } from '../services/auth';
-import AuthGuard from '../services/AuthGuard';
 import Header from '../components/Header';
 
 export default function RootLayout() {
@@ -24,16 +23,14 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <AuthProvider>
-        <AuthGuard>
-          <Header />
-          <Stack>
+        <Header />
+        <Stack>
             <Stack.Screen name="index" options={{ title: 'Home' }} />
             <Stack.Screen name="login" options={{ title: 'Login' }} />
             <Stack.Screen name="callback" options={{ headerShown: false }} />
             <Stack.Screen name="connected" options={{ title: 'Connected' }} />
             <Stack.Screen name="test-mood" options={{ title: 'Test Mood' }} />
           </Stack>
-        </AuthGuard>
       </AuthProvider>
       <StatusBar style="auto" />
     </ThemeProvider>
